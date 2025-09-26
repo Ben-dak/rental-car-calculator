9ac0fa3aa131e92ad1a7f6f89e15760a5c2e7a29
Dominick <dominickfrench.oe@gmail.com>
2025-09-26 15:13:59 -0500

    Second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,14 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print("Enter pickup date: ");
+        String pickupDate = myScanner.nextLine();
+
+        System.out.print("Do you want an electronic toll tag ($3.95/day)? (yes/no): ");
+
+        //I am still working on this - plan to spend time Saturday morning
 
 }
